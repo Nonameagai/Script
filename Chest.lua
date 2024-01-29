af789489c4b7943679a7ffcfbef549280b4e83dc
No <133481128+Nonameagai@users.noreply.github.com>
2024-01-29 18:41:02 +0700

    Chest.lua

--- Chest.lua
+++ Chest.lua
@@ -54,7 +54,7 @@ function TPReturner()
                     wait()
                     game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
                 end)
-                wait(1.6)
+                wait(2.2)
             end
         end
     end
